54ec6c51026a900a6c333ac1c5452a9baecafc11
Jakub Flaska <jakub.flaska@nih.gov>
2018-11-30 17:07:07 -0500

    x

--- server/modules/redditFunny/api/redditApi.js
+++ server/modules/redditFunny/api/redditApi.js
@@ -1,7 +1,6 @@
 const request = require('request');
 
 exports.getHot = (req, res)=>{
-    console.log('work');
     request('https://www.reddit.com/r/funny/hot.json', function (error, response, body) {
         let result = [];
         JSON.parse(body).data.children.forEach((redditPost)=>{
--- src/modules/redditFunny/redditPostList.component.js
+++ src/modules/redditFunny/redditPostList.component.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import {RedditPost} from './redditPost.component';
-
+import axios from 'axios';
 
 export class RedditPostList extends React.Component {
     state = {posts: [{title: 'aaa'}]};
+    constructor(props){
+        super(props);
+        axios.get('/api/redditFunny/getHot').then(response => {
+            console.log(response);
+            this.state.posts = response;
+        })
+    }
     render() {
         return (
             <div>
