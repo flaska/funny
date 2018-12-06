debbc514786da4ad9fb104bf18439a7e85531e3b
Jakub Flaska <jakub.flaska@nih.gov>
2018-12-06 14:59:15 -0500

    x

--- server/modules/redditFunny/api/redditApi.js
+++ server/modules/redditFunny/api/redditApi.js
@@ -3,12 +3,11 @@ const request = require('request');
 exports.getHot = (req, res)=>{
     request('https://www.reddit.com/r/funny/hot.json', function (error, response, body) {
         let result = [];
-        JSON.parse(body).data.children.forEach((redditPost)=>{
+        JSON.parse(body).data.children.filter(redditPost=>!redditPost.data.distinguished).forEach((redditPost)=>{
             let jsonPost = {
                 title: redditPost.data.title,
                 url: redditPost.data.url,
                 thumbnail : redditPost.data.thumbnail
-                // preview: redditPost.data.preview.images[0].source.url
             };
             result.push(jsonPost);
         });
