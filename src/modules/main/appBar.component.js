d2b5f65b0d960ecba5fb6224086fbb755ddbdead
Jakub Flaska <jakub.flaska@nih.gov>
2018-12-10 11:20:15 -0500

    x

---
+++ src/modules/main/appBar.component.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    root: {
+        flexGrow: 1,
+    },
+};
+
+export class SlackerAppBar extends React.Component {
+    render(){
+        return (
+            <div className={styles.root}>
+                <AppBar position="static" color="default">
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit">
+                            Photos
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
--- src/modules/main/main.component.js
+++ src/modules/main/main.component.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import 'typeface-roboto';
 import {FeedList} from "../feed/feedList.component";
+import {SlackerAppBar} from "./appBar.component";
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export class Main extends React.Component {
+    componentDidMount(){
+        document.title = "Slacker"
+    }
     render() {
         return (
-            <FeedList feedUrl={'/api/reddit/feed?subreddit=funny&channel=hot'}></FeedList>
+            <React.Fragment>
+                <CssBaseline/>
+                <SlackerAppBar></SlackerAppBar>
+                <FeedList feedUrl={'/api/reddit/feed?subreddit=funny&channel=hot'}></FeedList>
+            </React.Fragment>
         );
     }
 }
