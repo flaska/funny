03708720166f4829548b065d6a269713ca74c560
Jakub Flaska <jakub.flaska@nih.gov>
2018-12-10 15:06:53 -0500

    x

--- src/modules/comments/comment.component.js
+++ src/modules/comments/comment.component.js
@@ -15,16 +15,23 @@ export class Comment extends React.Component {
         this.state = {};
         if (this.props.commentData.body) this.state.body = this.props.commentData.body.replace(/\n/g, '<br>');
     }
-    render() {
-        return (
-            <div>
+    showBody(data){
+        if (!data.body) return(<div></div>);
+        return(
+            <React.Fragment>
                 <CommentMetadata commentData={this.props.commentData}/>
                 <Typography  dangerouslySetInnerHTML={{__html: this.state.body}}></Typography>
                 <Divider />
+            </React.Fragment>);
+    }
+    render() {
+        return (
+            <React.Fragment>
+                {this.showBody(this.props.commentData)}
                 <div style={styles.replies}>
                     {this.props.commentData.replies.map(r=><Comment commentData={r}></Comment>)}
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
 }
--- src/modules/comments/commentMetadata.component.js
+++ src/modules/comments/commentMetadata.component.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import {InfoChip} from "../utils/infoChip.component";
 
 export class CommentMetadata extends React.Component {
     render(){
         return(
             <Typography>
                 Score | {this.props.commentData.score}
+                <InfoChip/>
             </Typography>
         );
     }
