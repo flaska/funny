6e8803f7e421807d8f934231ced197c56129002f
Jakub Flaska <jakub.flaska@nih.gov>
2018-12-11 13:56:10 -0500

    x

--- src/modules/comments/commentsList.component.js
+++ src/modules/comments/commentsList.component.js
@@ -5,6 +5,7 @@ import axios from "axios/index";
 import {Comment} from './comment.component'
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 const styles = {
     buttons: {
@@ -16,14 +17,20 @@ const styles = {
 export class CommentsList extends React.Component {
     constructor(props){
         super(props);
-        this.state = {postId: this.props.postId};
+        this.state = {
+            postId: this.props.postId,
+            dataFetched: false
+        };
 
         axios.get('/api/reddit/comments?postId='+this.state.postId).then(response => {
-            this.setState({postId: this.state.postId, commentsRoot: response.data});
+            this.setState({postId: this.state.postId, commentsRoot: response.data, dataFetched: true});
         });
     }
+    handleClick(event){
+        this.props.onClick(event);
+    }
     render() {
-        if (!this.state.commentsRoot) return (<div></div>);
+        if (!this.state.dataFetched) return (<LinearProgress />);
         return (
             <Card>
                 <CardContent>
@@ -32,7 +39,7 @@ export class CommentsList extends React.Component {
                         <Typography style={styles.hint}>
                             Showing top comments...
                         </Typography>
-                        <Button onClick={()=>this.props.onClick('collapseComments')}>
+                        <Button onClick={()=>this.handleClick('collapseComments')}>
                             Close
                         </Button>
                         <Button color="primary" onClick={()=>this.props.onClick('seeAll')}>
