88314c27178b33879834ba434412a01453efdc9c
Jakub Flaska <jakub.flaska@nih.gov>
2018-12-11 15:16:55 -0500

    x

--- src/modules/feed/feedList.component.js
+++ src/modules/feed/feedList.component.js
@@ -3,46 +3,53 @@ import axios from 'axios';
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import {FeedItem} from './feedItem.component';
+import Button from '@material-ui/core/Button';
 
 const styles = {
-  list: {
+    main: {
       maxWidth: 760,
       margin: 'auto'
-  },
-  listItem: {
+    },
+    listItem: {
       display: 'block'
-  }
+    },
+    more: {
+        margin: 'auto',
+        display: 'block',
+        marginBottom: 50
+    }
 };
 
 export class FeedList extends React.Component {
-    state = {posts: [], chunkSize: 10};
+    chunkSize = 10;
+    state = {posts: []};
     loadMorePosts(){
-        axios.get(this.props.feedUrl + `&from=${this.state.posts.length}&size=${this.state.chunkSize}`).then(response => {
-            // this.state.posts = this.state.posts.concat(response.posts);
-            // this.state = response;
-            this.setState(response);
+        axios.get(this.props.feedUrl + `&from=${this.state.posts.length}&size=${this.chunkSize}`).then(response => {
+            if (this.state.posts.length==0) this.setState(response.data);
+            else {
+                this.state.posts = this.state.posts.concat(response.data.posts);
+                this.setState(this.state);
+            }
         });
     }
     constructor(props){
         super(props);
-        // this.loadMorePosts();
-
-        axios.get(this.props.feedUrl + `&from=${this.state.posts.length}&size=${this.state.chunkSize}`).then(response => {
-            // this.state.posts = this.state.posts.concat(response.posts);
-            // this.state = response;
-            this.setState(response);
-        });
+        this.loadMorePosts();
     }
     render() {
         return (
+            <div style={styles.main}>
                 <List style={styles.list}>
-                    xxx
                     {this.state.posts.map(post=>
                         <ListItem style={styles.listItem}>
                             <FeedItem postData={post}></FeedItem>
                         </ListItem>
                     )}
                 </List>
+                <Button style={styles.more} variant="contained" color="primary" onClick={()=>this.loadMorePosts()}>
+                    More Fun
+                </Button>
+            </div>
         );
     }
 }
