d932ba7e7a8713bc834190e04b6d3dbb04084565
Jakub Flaska <jakub.flaska@nih.gov>
2018-12-17 16:32:35 -0500

    x

--- cypress/integration/feedItemList/feedItemList.test.js
+++ cypress/integration/feedItemList/feedItemList.test.js
@@ -1,12 +1,20 @@
-// const slack = require('../shared/4slack.lib');
-
 describe('Feed List', function() {
-    it('Sees ten posts', function() {
+    it('Sees ten posts from /r/Funny', function() {
         cy.homepage();
         cy.get('#feedList').find('.feedItem').should('have.length', 10);
+        cy.request('https://www.reddit.com/r/funny/hot.json').then((response)=>{
+            let redditData = response.body.data;
+            cy.get('#feedList .feedItem').first().contains(redditData.children[1].data.title);
+            cy.get('#feedList .feedItem').eq(1).contains(redditData.children[2].data.title);
+        });
     });
-    it('Can see more posts', ()=>{
+    it('Can see more posts from /r/Funny', ()=>{
         cy.contains('More Fun').click();
-        cy.get('#feedList').find('.feedItem').should('have.length', 20)
+        cy.get('#feedList').find('.feedItem').should('have.length', 20);
+
+        cy.request('https://www.reddit.com/r/funny/hot.json').then((response)=>{
+            let redditData = response.body.data;
+            cy.get('#feedList .feedItem').eq(10).contains(redditData.children[11].data.title);
+        });
     });
 });
--- server/modules/reddit/deliver/redditRoutes.js
+++ server/modules/reddit/deliver/redditRoutes.js
@@ -1,9 +1,6 @@
 const router = require('express').Router(),
-    cache = require('apicache').middleware('15 minutes'),
     redditDb = require('../db/redditDb')
 ;
-
-router.use(cache);
 
 router.get('/feed', (req, res)=>{
     if (!req.query.subreddit || !req.query.channel || !req.query.from || !req.query.size) return res.status(404).send('Please specify all parameters');
