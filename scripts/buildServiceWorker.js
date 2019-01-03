a4779ac0042877c55d6b2597c900c708027369b9
Jakub Flaska <jakub.flaska@nih.gov>
2019-01-03 15:38:50 -0500

    x

--- scripts/buildServiceWorker.js
+++ scripts/buildServiceWorker.js
@@ -1,6 +1,6 @@
 const testFolder = './build/';
 const fs = require('fs');
 
-fs.readdirSync(testFolder).filter().forEach(file => {
+fs.readdirSync(testFolder).filter(f=>f.indexOf('precache')===-0).forEach(file => {
     console.log(file);
 });
