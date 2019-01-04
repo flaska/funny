fed663a3c66f7041015f4e0d583f008eee477488
Jakub Flaska <jakub.flaska@nih.gov>
2019-01-04 13:48:25 -0500

    'x'

---
+++ build/precache-manifest.60fb469fffa8e8952368d003ebfeff31.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dff6d638ae00ef9e3cf",
+    "url": "/static/js/6.6dff6d63.chunk.js"
+  },
+  {
+    "revision": "38bbba044c41decdddec",
+    "url": "/static/js/main.38bbba04.chunk.js"
+  },
+  {
+    "revision": "2dd3c5112a1fe969a395",
+    "url": "/static/js/runtime~main.2dd3c511.js"
+  },
+  {
+    "revision": "a641be4bca18d67fe86b",
+    "url": "/static/js/9.a641be4b.chunk.js"
+  },
+  {
+    "revision": "1bad9d3164cf2226131f",
+    "url": "/static/js/8.1bad9d31.chunk.js"
+  },
+  {
+    "revision": "f29c5bd5005a6fc44bd9",
+    "url": "/static/js/7.f29c5bd5.chunk.js"
+  },
+  {
+    "revision": "b4e58ff8881ab54c4272",
+    "url": "/static/js/5.b4e58ff8.chunk.js"
+  },
+  {
+    "revision": "d476b12a5dee6981e8d0",
+    "url": "/static/js/4.d476b12a.chunk.js"
+  },
+  {
+    "revision": "9e9d9eabe8da38c1876a",
+    "url": "/static/js/3.9e9d9eab.chunk.js"
+  },
+  {
+    "revision": "10c611048ab4eb0c500f",
+    "url": "/static/js/2.10c61104.chunk.js"
+  },
+  {
+    "revision": "568805cd253253a1948b",
+    "url": "/static/js/10.568805cd.chunk.js"
+  },
+  {
+    "revision": "9b44c3df29358f0ed890",
+    "url": "/static/js/1.9b44c3df.chunk.js"
+  },
+  {
+    "revision": "20e4d16c356f495eabc361fffdb5166a",
+    "url": "/index.html"
+  }
+];
--- build/slackServiceWorker.js
+++ build/slackServiceWorker.js
@@ -1,8 +1,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const cacheName = 'precache-manifest.7f79d90bda61a03a831ac9cac37f684e.js';
-importScripts("precache-manifest.7f79d90bda61a03a831ac9cac37f684e.js");
+const cacheName = 'precache-manifest.60fb469fffa8e8952368d003ebfeff31.js';
+importScripts("precache-manifest.60fb469fffa8e8952368d003ebfeff31.js");
 const urlsToCache = self.__precacheManifest.map(e=>e.url);
 
 // The install handler takes care of precaching the resources we always need.
