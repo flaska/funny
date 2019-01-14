cd953c64ccdb5cb5f3de6ff2f1c7151dbd13f80c
Jakub Flaska <jakub.flaska@nih.gov>
2019-01-14 13:49:20 -0500

    x

--- cypress/integration/topBar/switchChannel.test.js
+++ cypress/integration/topBar/switchChannel.test.js
@@ -8,6 +8,11 @@ describe('Switch Channel Test', function() {
 
     it('Can switch to Top / Week', function() {
         cy.switchChannelTo('topWeek');
+        cy.getFeed('funny', 'topweek', 0, 10).then((response)=>{
+            let posts = response.body.posts;
+            cy.checkPostTitleByIndex(0, posts[0].title);
+            cy.checkPostTitleByIndex(1, posts[1].title);
+        });
     });
 
 });
